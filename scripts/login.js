2593a85702fbcf7df72ebc266f5709989b72e7d2
lsq6815 <lsq6815@gmail.com>
2020-12-25 02:24:19 -0500

    able to login in with username and password now. use ajax and JSON to verify the login in. able to tell which item(name/passwd) is incorrect if fail to login in.

--- scripts/login.js
+++ scripts/login.js
@@ -11,19 +11,21 @@ document.querySelector("form#pwd_login a.login_button").onclick = function() {
         if (this.readyState == 4 && this.status == 200) {
             // parse response as JSON
             var json = JSON.parse(this.responseText);
+            console.log(json);
             if (!json.status) {
                 // if password is incorrect, show error tip
+                document.getElementById('show_error').innerText = json.error;
                 document.getElementById('show_error').style.opacity = 1;
             } else {
                 // TODO: jamp to the success login page
-                alert("Your account and password is correct, but the login is under construction");
                 document.getElementById('show_error').style.opacity = 0;
+                alert("Password correct");
             }
         }
     };
 
     // send data as JSON
-    xmlhttp.open("POST", "demo.php", true);
+    xmlhttp.open("POST", "./pwd_login.php", true);
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send(JSON.stringify(login));
 }
