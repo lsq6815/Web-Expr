0c3aded5826708c147bb0a357d5c10a378d0b74a
lsq6815 <lsq6815@gmail.com>
2020-12-26 02:54:11 -0500

    ADD password change feature

--- login.php
+++ login.php
@@ -16,6 +16,25 @@ if (isset($_GET['username'])) {
     echo "You give " . $_GET['token'] . "<br/>";
     if ($_GET['token'] == $_SESSION[$_GET['username']]) {
         echo "LOGIN!<br/>";
+        echo <<<EOF
+<h1>Change Password</h1>
+<form method="GET" action="password_change.php">
+    <table>
+        <tr>
+            <td><input type="hidden" size="10" name="username" value="{$_GET['username']}"></td>
+        </tr>
+        <tr>
+            <td><input type="hidden" size="10" name="token" value="{$_GET['token']}"></td>
+        </tr>
+        <tr>
+            <td>Enter your new password:</td>
+            <td><input type="password" size="10" name="newpassword"></td>
+        </tr>
+    </table>
+    <p><input type="submit" value="Update Password"></p>
+</form>
+<p><a href="index.html">HOME</a>
+EOF;
     }
     else {
         echo "Wrong token<br/>";
